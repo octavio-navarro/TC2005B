cdc6087fc00eaf495239d56f5e1a4254f35185f9
Gilberto Echeverria <gilbertoef@gmail.com>
2022-03-02 17:46:26 -0600

    Allow the player to jump

--- Videogames/TC2005B.400-202211/Assets/Scripts/RBMotion.cs
+++ Videogames/TC2005B.400-202211/Assets/Scripts/RBMotion.cs
@@ -8,7 +8,9 @@ using UnityEngine;
 
 public class RBMotion : MonoBehaviour
 {
-    [SerializeField] float force;
+    // Jumping force
+    [SerializeField] Vector2 force;
+    // Walking speed
     [SerializeField] float speed;
 
     Rigidbody2D rb2D;
@@ -17,17 +19,23 @@ public class RBMotion : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Get a reference to the Rigid body to change physics
         rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //move.x = Input.GetAxis("Horizontal") * force;
-        //rb2D.AddForce(move);
-
+        // Move horizontally
         move.x = Input.GetAxis("Horizontal") * speed;
         move.y = rb2D.velocity.y;
+        // By setting the velocity, the player reacts instantly to input
         rb2D.velocity = move;
+
+        if (Input.GetButtonDown("Jump")) {
+            // By applying a force, the player can use physics to alter
+            //  the velocity
+            rb2D.AddForce(force);
+        }
     }
 }
