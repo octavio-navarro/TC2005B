6266a5c979a82ef2a97078d9f5cfd4ff0e253d1c
Gilberto Echeverria <gilbertoef@gmail.com>
2022-04-28 18:34:34 -0500

    Update the API example with more comments and functions

--- Videogames/TC2005B.400-202211/Assets/Scripts/APITest.cs
+++ Videogames/TC2005B.400-202211/Assets/Scripts/APITest.cs
@@ -11,6 +11,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+
+// Create classes that correspond to the data that will be sent/received
+// via the API
+
 // Allow the class to be extracted from Unity
 // https://stackoverflow.com/questions/40633388/show-members-of-a-class-in-unity3d-inspector
 [System.Serializable]
@@ -40,34 +44,50 @@ public class APITest : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space)) {
-            StartCoroutine(GetUsers());
+            QueryUsers();
         }
         if (Input.GetKeyDown(KeyCode.N)) {
-            StartCoroutine(AddUser());
+            InsertNewUser();
         }
     }
 
+    // These are the functions that must be called to interact with the API
+
+    public void QueryUsers()
+    {
+        StartCoroutine(GetUsers());
+    }
+
+    public void InsertNewUser()
+    {
+        StartCoroutine(AddUser());
+    }
+
+    // These functions make the connection to the API
+
     IEnumerator GetUsers()
     {
-        UnityWebRequest www = UnityWebRequest.Get(url + getUsersEP);
-        yield return www.SendWebRequest();
+        using (UnityWebRequest www = UnityWebRequest.Get(url + getUsersEP))
+        {
+            yield return www.SendWebRequest();
 
-        if (www.result == UnityWebRequest.Result.Success) {
-            //Debug.Log("Response: " + www.downloadHandler.text);
-            // Compose the response to look like the object we want to extract
-            // https://answers.unity.com/questions/1503047/json-must-represent-an-object-type.html
-            string jsonString = "{\"users\":" + www.downloadHandler.text + "}";
-            allUsers = JsonUtility.FromJson<UserList>(jsonString);
-            DisplayUsers();
-        } else {
-            Debug.Log("Error: " + www.error);
+            if (www.result == UnityWebRequest.Result.Success) {
+                //Debug.Log("Response: " + www.downloadHandler.text);
+                // Compose the response to look like the object we want to extract
+                // https://answers.unity.com/questions/1503047/json-must-represent-an-object-type.html
+                string jsonString = "{\"users\":" + www.downloadHandler.text + "}";
+                allUsers = JsonUtility.FromJson<UserList>(jsonString);
+                DisplayUsers();
+            } else {
+                Debug.Log("Error: " + www.error);
+            }
         }
     }
 
     IEnumerator AddUser()
     {
         /*
-        // This should work with an API that does not expect JSON
+        // This should work with an API that does NOT expect JSON
         WWWForm form = new WWWForm();
         form.AddField("name", "newGuy" + Random.Range(1000, 9000).ToString());
         form.AddField("surname", "Tester" + Random.Range(1000, 9000).ToString());
@@ -78,23 +98,25 @@ public class APITest : MonoBehaviour
         User testUser = new User();
         testUser.name = "newGuy" + Random.Range(1000, 9000).ToString();
         testUser.surname = "Tester" + Random.Range(1000, 9000).ToString();
-
         //Debug.Log("USER: " + testUser);
         string jsonData = JsonUtility.ToJson(testUser);
         //Debug.Log("BODY: " + jsonData);
+
         // Send using the Put method:
         // https://stackoverflow.com/questions/68156230/unitywebrequest-post-not-sending-body
-        UnityWebRequest www = UnityWebRequest.Put(url + getUsersEP, jsonData);
-        //UnityWebRequest www = UnityWebRequest.Post(url + getUsersEP, form);
-        // Set the method later, and indicate the encoding is JSON
-        www.method = "POST";
-        www.SetRequestHeader("Content-Type", "application/json");
-        yield return www.SendWebRequest();
+        using (UnityWebRequest www = UnityWebRequest.Put(url + getUsersEP, jsonData))
+        {
+            //UnityWebRequest www = UnityWebRequest.Post(url + getUsersEP, form);
+            // Set the method later, and indicate the encoding is JSON
+            www.method = "POST";
+            www.SetRequestHeader("Content-Type", "application/json");
+            yield return www.SendWebRequest();
 
-        if (www.result == UnityWebRequest.Result.Success) {
-            Debug.Log("Response: " + www.downloadHandler.text);
-        } else {
-            Debug.Log("Error: " + www.error);
+            if (www.result == UnityWebRequest.Result.Success) {
+                Debug.Log("Response: " + www.downloadHandler.text);
+            } else {
+                Debug.Log("Error: " + www.error);
+            }
         }
     }
 
