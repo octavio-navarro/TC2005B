118bfa960b7210179cb60361c977438c0a445195
Gilberto Echeverria <gilbertoef@gmail.com>
2025-03-03 11:46:32 -0600

    Finished Pong game

--- VideogamesJS/js/04_pong.js
+++ VideogamesJS/js/04_pong.js
@@ -12,6 +12,12 @@ const canvasWidth = 800;
 const canvasHeight = 600;
 
 let oldTime;
+const paddleVelocity = 1.3;
+const speedIncrease = 1.1;
+const initialSpeed = 0.5;
+
+let leftScore = 0;
+let rightScore = 0;
 
 // Context of the Canvas
 let ctx;
@@ -20,16 +26,54 @@ let ctx;
 class Ball extends GameObject {
     constructor(position, width, height, color) {
         super(position, width, height, color, "ball");
-        this.velocity = new Vec(0.01, 0.01);
+        this.initVelocity();
+        this.inPlay = false;
     }
 
     update(deltaTime) {
         this.position = this.position.plus(this.velocity.times(deltaTime));
     }
+
+    initVelocity() {
+        this.inPlay = true;
+        let angle = Math.random() * (Math.PI / 2) - (Math.PI / 4);
+        this.velocity = new Vec(Math.cos(angle), Math.sin(angle)).times(initialSpeed);
+    }
+
+    reset() {
+        this.inPlay = false;
+        this.position = new Vec(canvasWidth / 2, canvasHeight / 2);
+        this.velocity = new Vec(0, 0);
+    }
+}
+
+class Paddle extends GameObject {
+    constructor(position, width, height, color) {
+        super(position, width, height, color, "paddle");
+        this.velocity = new Vec(0.0, 0.0);
+    }
+
+    update(deltaTime) {
+        this.position = this.position.plus(this.velocity.times(deltaTime));
+
+        if (this.position.y < 0) {
+            this.position.y = 0;
+        } else if (this.position.y + this.height > canvasHeight) {
+            this.position.y = canvasHeight - this.height;
+        }
+    }
 }
 
 // An object to represent the box to be displayed
-const box = new Ball(new Vec(0, canvasHeight / 2), 20, 20, "red");
+const box = new Ball(new Vec(canvasWidth / 2, canvasHeight / 2), 20, 20, "red");
+const leftPaddle = new Paddle(new Vec(20, canvasHeight / 2), 20, 100, "blue");
+const rightPaddle = new Paddle(new Vec(canvasWidth - 40, canvasHeight / 2), 20, 100, "blue");
+const topBar = new GameObject(new Vec(0, 0), canvasWidth, 20, "black", "obstacle");
+const bottomBar = new GameObject(new Vec(0, canvasHeight - 20), canvasWidth, 20, "black", "obstacle");
+const leftGoal = new GameObject(new Vec(0, 0), 20, canvasHeight, "green", "leftGoal");
+const rightGoal = new GameObject(new Vec(canvasWidth -20, 0), 20, canvasHeight, "green", "rightGoal");
+const leftLabel = new TextLabel(100, 50, "40px Ubuntu Mono", "white")
+const rightLabel = new TextLabel(500, 50, "40px Ubuntu Mono", "white")
 
 
 function main() {
@@ -41,9 +85,39 @@ function main() {
     // Get the context for drawing in 2D
     ctx = canvas.getContext('2d');
 
-
+    createEventListeners();
 
     drawScene(0);
+}
+
+function createEventListeners() {
+    window.addEventListener('keydown', (event) => {
+        if (event.key == 'q') {
+            leftPaddle.velocity = new Vec(0, -paddleVelocity);
+        } else if (event.key == 'a') {
+            leftPaddle.velocity = new Vec(0, paddleVelocity);
+        } else if (event.key == 'o' || event.code == 'ArrowUp') {
+            rightPaddle.velocity = new Vec(0, -paddleVelocity);
+        } else if (event.key == 'l' || event.code == 'ArrowDown') {
+            rightPaddle.velocity = new Vec(0, paddleVelocity);
+        }
+    });
+
+    window.addEventListener('keyup', (event) => {
+        if (event.key == 'q') {
+            leftPaddle.velocity = new Vec(0, 0);
+        } else if (event.key == 'a') {
+            leftPaddle.velocity = new Vec(0, 0);
+        } else if (event.key == 'o' || event.code == 'ArrowUp') {
+            rightPaddle.velocity = new Vec(0, 0);
+        } else if (event.key == 'l' || event.code == 'ArrowDown') {
+            rightPaddle.velocity = new Vec(0, 0);
+        }
+
+        if (event.key == 's' && !box.inPlay) {
+            box.initVelocity();
+        }
+    });
 }
 
 function drawScene(newTime) {
@@ -56,22 +130,38 @@ function drawScene(newTime) {
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
     // Draw a square
+    leftLabel.draw(ctx, `${leftScore}`);
+    rightLabel.draw(ctx, `${rightScore}`);
+    leftGoal.draw(ctx);
+    rightGoal.draw(ctx);
+    topBar.draw(ctx);
+    bottomBar.draw(ctx);
+    leftPaddle.draw(ctx);
+    rightPaddle.draw(ctx);
     box.draw(ctx);
 
     //console.log(`DeltaTime: ${deltaTime}`);
     // Update the properties of the object
     box.update(deltaTime);
+    leftPaddle.update(deltaTime);
+    rightPaddle.update(deltaTime);
 
-    /*
-    // Make the ball bounde at the boundaries
-    if (box.position.x > canvasWidth - box.width) {
-        box.direction = -1;
-        box.speed += 0.1;
-    } else if (box.position.x < 0) {
-        box.direction = 1;
-        box.speed += 0.1;
+    if (boxOverlap(box, leftPaddle) || boxOverlap(box, rightPaddle)) {
+        box.velocity.x *= -1;
+        box.velocity = box.velocity.times(speedIncrease);
     }
-    */
+    if (boxOverlap(box, topBar) || boxOverlap(box, bottomBar)) {
+        box.velocity.y *= -1;
+        box.velocity = box.velocity.times(speedIncrease);
+    }
+    if (boxOverlap(box, leftGoal)) {
+        rightScore += 1;
+        box.reset();
+    }
+    if (boxOverlap(box, rightGoal)) {
+        leftScore += 1;
+        box.reset();
+    }
 
     oldTime = newTime;
     requestAnimationFrame(drawScene);
--- VideogamesJS/js/game_classes.js
+++ VideogamesJS/js/game_classes.js
@@ -62,3 +62,26 @@ class GameObject {
 
     }
 }
+
+class TextLabel {
+    constructor(x, y, font, color) {
+        this.x = x;
+        this.y = y;
+        this.font = font;
+        this.color = color;
+    }
+
+    draw(ctx, text) {
+        ctx.font = this.font;
+        ctx.fillStyle = this.color;
+        ctx.fillText(text, this.x, this.y);
+    }
+}
+
+// Detect a collision of two box objects
+function boxOverlap(obj1, obj2) {
+    return obj1.position.x + obj1.width > obj2.position.x &&
+           obj1.position.x < obj2.position.x + obj2.width &&
+           obj1.position.y + obj1.height > obj2.position.y &&
+           obj1.position.y < obj2.position.y + obj2.height;
+}
