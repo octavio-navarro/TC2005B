7952946e908d5df528349b0be0ddb2a26e7497cb
Gilberto Echeverria <gilbertoef@gmail.com>
2025-03-12 11:47:37 -0600

    Add 2D examples

---
+++ VideogamesJS/Examples/05_Platformer/js/game_classes.js
@@ -0,0 +1,154 @@
+/*
+ * General classes that can be useful for a game
+ *
+ * Gilberto Echeverria
+ * 2025-01-22
+ */
+
+"use strict";
+
+class Vec {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    plus(other) {
+        return new Vec(this.x + other.x, this.y + other.y);
+    }
+
+    minus(other) {
+        return new Vec(this.x - other.x, this.y - other.y);
+    }
+
+    times(factor) {
+        return new Vec(this.x * factor, this.y * factor);
+    }
+
+    get length() {
+        return Math.sqrt(this.x ** 2 + this.y ** 2);
+    }
+}
+
+
+class Rect {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
+
+
+class GameObject {
+    constructor(color, width, height, x, y, type) {
+        this.position = new Vec(x, y);
+        this.size = new Vec(width, height);
+        this.color = color;
+        this.type = type;
+
+        // Sprite properties
+        this.spriteImage = undefined;
+        this.spriteRect = undefined;
+    }
+
+    setSprite(imagePath, rect) {
+        this.spriteImage = new Image();
+        this.spriteImage.src = imagePath;
+        if (rect) {
+            this.spriteRect = rect;
+        }
+    }
+
+    draw(ctx, scale) {
+        if (this.spriteImage) {
+            // Draw a sprite if the object has one defined
+            if (this.spriteRect) {
+                ctx.drawImage(this.spriteImage,
+                              this.spriteRect.x * this.spriteRect.width,
+                              this.spriteRect.y * this.spriteRect.height,
+                              this.spriteRect.width, this.spriteRect.height,
+                              this.position.x * scale, this.position.y * scale,
+                              this.size.x * scale, this.size.y * scale);
+            } else {
+                ctx.drawImage(this.spriteImage,
+                              this.position.x * scale, this.position.y * scale,
+                              this.size.x * scale, this.size.y * scale);
+            }
+        } else {
+            // If there is no sprite asociated, just draw a color square
+            ctx.fillStyle = this.color;
+            ctx.fillRect(this.position.x * scale, this.position.y * scale,
+                         this.size.x * scale, this.size.y * scale);
+        }
+    }
+
+    update() {
+
+    }
+}
+
+class AnimatedObject extends GameObject {
+    constructor(color, width, height, x, y, type) {
+        super(color, width, height, x, y, type);
+        // Animation properties
+        this.frame = 0;
+        this.minFrame = 0;
+        this.maxFrame = 0;
+        this.sheetCols = 0;
+
+        this.repeat = true;
+
+        // Delay between frames (in milliseconds)
+        this.frameDuration = 100;
+        this.totalTime = 0;
+    }
+
+    setAnimation(minFrame, maxFrame, repeat, duration) {
+        this.minFrame = minFrame;
+        this.maxFrame = maxFrame;
+        this.frame = minFrame;
+        this.repeat = repeat;
+        this.totalTime = 0;
+        this.frameDuration = duration;
+    }
+
+    updateFrame(deltaTime) {
+        this.totalTime += deltaTime;
+        if (this.totalTime > this.frameDuration) {
+            // Loop around the animation frames if the animation is set to repeat
+            // Otherwise stay on the last frame
+            let restartFrame = (this.repeat ? this.minFrame : this.frame);
+            this.frame = this.frame < this.maxFrame ? this.frame + 1 : restartFrame;
+            this.spriteRect.x = this.frame % this.sheetCols;
+            this.spriteRect.y = Math.floor(this.frame / this.sheetCols);
+            this.totalTime = 0;
+        }
+    }
+}
+
+
+class TextLabel {
+    constructor(x, y, font, color) {
+        this.x = x;
+        this.y = y;
+        this.font = font;
+        this.color = color;
+    }
+
+    draw(ctx, text) {
+        ctx.font = this.font;
+        ctx.fillStyle = this.color;
+        ctx.fillText(text, this.x, this.y);
+    }
+}
+
+
+// Simple collision detection between rectangles
+function overlapRectangles(actor1, actor2) {
+    return actor1.position.x + actor1.size.x > actor2.position.x &&
+           actor1.position.x < actor2.position.x + actor2.size.x &&
+           actor1.position.y + actor1.size.y > actor2.position.y &&
+           actor1.position.y < actor2.position.y + actor2.size.y;
+}
