9cb62959566157dede69dbfed87820bb7fbcb93c
Gilberto Echeverria <gilbertoef@gmail.com>
2025-04-09 09:10:10 -0600

    Add basic gameplay to Pong game

--- VideogamesJS/js/04_pong.js
+++ VideogamesJS/js/04_pong.js
@@ -28,11 +28,16 @@ let game;
 // Clases for the Pong game
 class Ball extends GameObject {
     constructor(position, width, height, color) {
+        // Call the parent's constructor
+        super(position, width, height, color, "ball");
 
+        // ONLY TEMPORARY
+        this.velocity = new Vec(initialSpeed, initialSpeed);
     }
 
     update(deltaTime) {
-
+        // Change the position depending on the velocity
+        this.position = this.position.plus(this.velocity.times(deltaTime));
     }
 
     initVelocity() {
@@ -46,11 +51,20 @@ class Ball extends GameObject {
 
 class Paddle extends GameObject {
     constructor(position, width, height, color) {
-
+        super(position, width, height, color, "paddle");
+        this.velocity = new Vec(0, 0);
     }
 
     update(deltaTime) {
+        // Change the position depending on the velocity
+        this.position = this.position.plus(this.velocity.times(deltaTime));
 
+        // Constrain the motion to be within the canvas size
+        if (this.position.y < 0) {
+            this.position.y = 0;
+        } else if (this.position.y + this.height > canvasHeight) {
+            this.position.y = canvasHeight - this.height;
+        }
     }
 }
 
@@ -58,27 +72,89 @@ class Paddle extends GameObject {
 class Game {
     constructor(canvasWidth, canvasHeight) {
         // Create instances for all objects in the game
+        this.ball = new Ball(new Vec(canvasWidth / 2, canvasHeight / 2),
+                             20, 20, "black");
+        this.paddleLeft = new Paddle(new Vec(20, canvasHeight / 2 - 50),
+                             20, 100, "green");
+        this.paddleRight = new Paddle(new Vec(canvasWidth - 20 - 20, canvasHeight / 2 - 50),
+                             20, 100, "green");
+        this.topBorder = new GameObject(new Vec(0, 0), canvasWidth, 10, "red", "barrier");
+        this.bottomBorder = new GameObject(new Vec(0, canvasHeight - 10), canvasWidth, 10, "red", "barrier");
+        this.goalLeft = new GameObject(new Vec(0, 0), 10, canvasHeight, "white", "goal");
+        this.goalRight = new GameObject(new Vec(canvasWidth-10, 0), 10, canvasHeight, "white", "goal");
+
+        this.pointsLeft = 0;
+        this.pointsRight = 0;
 
         this.createEventListeners();
     }
 
     update(deltaTime) {
+        this.paddleLeft.update(deltaTime);
+        this.paddleRight.update(deltaTime);
+        this.ball.update(deltaTime);
 
+        if (boxOverlap(this.ball, this.paddleRight) ||
+            boxOverlap(this.ball, this.paddleLeft)) {
+            this.ball.velocity.x *= -1;
+        }
+
+        if (boxOverlap(this.ball, this.topBorder) ||
+            boxOverlap(this.ball, this.bottomBorder)) {
+            this.ball.velocity.y *= -1;
+        }
+
+        if (boxOverlap(this.ball, this.goalLeft)) {
+            this.pointsRight += 1;
+            console.log(`Score ${this.pointsLeft}, ${this.pointsRight}`);
+        }
+
+        if (boxOverlap(this.ball, this.goalRight)) {
+            this.pointsLeft += 1;
+            console.log(`Score ${this.pointsLeft}, ${this.pointsRight}`);
+        }
     }
 
     draw(ctx) {
         // Draw all objects in the game
         // Draw from back to front, so objects are not overpainted
-
+        this.topBorder.draw(ctx);
+        this.bottomBorder.draw(ctx);
+        this.paddleLeft.draw(ctx);
+        this.paddleRight.draw(ctx);
+        this.ball.draw(ctx);
     }
 
     createEventListeners() {
         window.addEventListener('keydown', (event) => {
+            if (event.key === "q") {
+                this.paddleLeft.velocity.y = -paddleVelocity;
+            }
+            if (event.key === "a") {
+                this.paddleLeft.velocity.y = paddleVelocity;
+            }
+            if (event.key === "o") {
+                this.paddleRight.velocity.y = -paddleVelocity;
+            }
+            if (event.key === "l") {
+                this.paddleRight.velocity.y = paddleVelocity;
+            }
 
         });
 
         window.addEventListener('keyup', (event) => {
-
+            if (event.key === "q") {
+                this.paddleLeft.velocity.y = 0;
+            }
+            if (event.key === "a") {
+                this.paddleLeft.velocity.y = 0;
+            }
+            if (event.key === "o") {
+                this.paddleRight.velocity.y = 0;
+            }
+            if (event.key === "l") {
+                this.paddleRight.velocity.y = 0;
+            }
         });
     }
 }
---
+++ VideogamesJS/js/game_object_class.js
@@ -0,0 +1,32 @@
+/*
+ * Base class for game objects in general
+ *
+ * Gilberto Echeverria
+ * 2025-04-07
+ */
+
+class GameObject {
+    constructor(position, width, height, color, type) {
+        this.position = position;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.type = type;
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+
+    update(deltaTime) {
+
+    }
+}
+
+function boxOverlap(rect1, rect2) {
+    return rect1.position.x < rect2.position.x + rect2.width &&
+           rect1.position.x + rect1.width > rect2.position.x &&
+           rect1.position.y < rect2.position.y + rect2.height &&
+           rect1.position.y + rect1.height > rect2.position.y;
+}
